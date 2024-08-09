d053972c796dafe9e837bacdf9e10af01e9a3413
Dev-ChandanTomar <chandan.nextwork@gmail.com>
2024-08-09 17:27:33 +0530

    tokens added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+// const asyncHandler=()=>{}
+
+export {asyncHandler}
+
+const asyncHandler=(fun)=async(req,res,next)=>{
+try {
+    await fun(req,res,next)
+} catch (error) {
+    res.status(err.code || 500).json({
+        success:false,
+        message:err.message
+    })
+    
+}
+
+}
